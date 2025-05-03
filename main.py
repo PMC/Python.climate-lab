ff7955ef1a2b70ff7cf24781999a97fb454fe378
Pierre Chateau <pierre@chateau.se>
2025-05-03 11:23:08 +0200

    updated output

--- main.py
+++ main.py
@@ -9,11 +9,6 @@ from sklearn.preprocessing import OneHotEncoder
 def main():
     data = pd.read_csv(f"data\Temperature_Celsius_5Col-2025.csv")
 
-    # We should scale on X and Y separately
-    # need to preserve negative values ? MinMaxScaler(feature_range=(-1, 1)).
-    x_scaler = MinMaxScaler()
-    y_scaler = MinMaxScaler()
-
     # split the data into training and testing data
     X = data.drop(columns=["label"])
     y = data["label"]
@@ -21,25 +16,30 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
     # scale inputs (X)
-    X_train_scaled = x_scaler.fit_transform(X_train)
+    # use X since there can be a difference in train and test
+    # need to preserve negative values ? MinMaxScaler(feature_range=(-1, 1)).
+    x_scaler = MinMaxScaler().fit(X)
+    X_train_scaled = x_scaler.transform(X_train)
     X_test_scaled = x_scaler.transform(X_test)
 
+    print(f"x_scaler min: {x_scaler.data_min_}")
+    print(f"x_scaler max: {x_scaler.data_max_}")
+    print(X.describe())
+
     # Optionally, scale outputs (y)
-    y_train_scaled = y_scaler.fit_transform(y_train.values.reshape(-1, 1))
+    y_scaler = MinMaxScaler().fit(y.values.reshape(-1, 1))
+    y_train_scaled = y_scaler.transform(y_train.values.reshape(-1, 1))
     y_test_scaled = y_scaler.transform(y_test.values.reshape(-1, 1))
 
+    print("###")
+    print(f"y_scaler min: {y_scaler.data_min_}")
+    print(f"y_scaler max: {y_scaler.data_max_}")
+    print(y.describe())
     # if keras need a dataframe then use:
     # X_train_scaled_df = pd.DataFrame(X_train_scaled, columns=X.columns)
 
-    # print("###")
-    print(X_train_scaled)
-    print("###")
-    print(y_train_scaled)
-    # print("###")
-    # print(y_train)
-
-    plt.plot(X_test_scaled)
-    plt.show()
+    # plt.plot(X_test_scaled)
+    # plt.show()
 
 
 if __name__ == "__main__":
