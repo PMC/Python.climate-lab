6db2dd89819fd9dfd7f23988f7328196b2733569
Pierre Chateau <pierre@chateau.se>
2025-05-03 11:42:57 +0200

    restore columns

--- main.py
+++ main.py
@@ -19,22 +19,30 @@ def main():
     # use X since there can be a difference in train and test
     # need to preserve negative values ? MinMaxScaler(feature_range=(-1, 1)).
     x_scaler = MinMaxScaler().fit(X)
-    X_train_scaled = x_scaler.transform(X_train)
-    X_test_scaled = x_scaler.transform(X_test)
+    X_train_scaled = pd.DataFrame(x_scaler.transform(X_train), columns=X.columns)
+    X_test_scaled = pd.DataFrame(x_scaler.transform(X_test), columns=X.columns)
 
     print(f"x_scaler min: {x_scaler.data_min_}")
     print(f"x_scaler max: {x_scaler.data_max_}")
     print(X.describe())
+    print("###")
+    print(X_train_scaled)
 
     # Optionally, scale outputs (y)
     y_scaler = MinMaxScaler().fit(y.values.reshape(-1, 1))
-    y_train_scaled = y_scaler.transform(y_train.values.reshape(-1, 1))
-    y_test_scaled = y_scaler.transform(y_test.values.reshape(-1, 1))
+    y_train_scaled = pd.DataFrame(
+        y_scaler.transform(y_train.values.reshape(-1, 1)), columns=["label"]
+    )
+    y_test_scaled = pd.DataFrame(
+        y_scaler.transform(y_test.values.reshape(-1, 1)), columns=["label"]
+    )
 
     print("###")
     print(f"y_scaler min: {y_scaler.data_min_}")
     print(f"y_scaler max: {y_scaler.data_max_}")
     print(y.describe())
+    print("###")
+    print(y_train_scaled)
     # if keras need a dataframe then use:
     # X_train_scaled_df = pd.DataFrame(X_train_scaled, columns=X.columns)
 
